61f3fe0be2e1e3f9f7c091f43daadc1bc2c8ac6b
鈴木慎乃介 <suzukimakotonosuke@suzukimukenoMBP.lan>
2023-01-17 22:06:57 +0900

    途中

--- todo-list/src/App.tsx
+++ todo-list/src/App.tsx
@@ -1,8 +1,32 @@
-import './App.css';
+import { useState } from "react";
 
 function App() {
+  //todoの追加
+  const [todo, setTodo] = useState("");
+
   return (
     <div className="App">
+      <h1>Todoリスト中級編</h1>
+        <form>
+          <p>タイトル</p>
+          <input name="title" type="text" placeholder="タイトル"/><br/>
+          <p>詳細</p>
+          <textarea name="detail" placeholder="todoの詳細"/><br/>
+          <button>追加</button>
+        </form>
+      <div>
+        <h2>Todo一覧</h2>
+        <table>
+          <thead>
+            <tr>
+              <td>ID</td>
+              <td>タイトル</td>
+              <td>詳細</td>
+              <td>状態</td>
+            </tr>
+          </thead>
+        </table>
+      </div>
     </div>
   );
 }
