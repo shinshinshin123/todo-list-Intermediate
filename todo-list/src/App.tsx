4419920d6d9a661293097b3cbf0e09d10b344bb9
鈴木慎乃介 <suzukimakotonosuke@suzukimukenoMBP.lan>
2023-01-25 21:40:15 +0900

    一旦コメントアウトする

--- todo-list/src/App.tsx
+++ todo-list/src/App.tsx
@@ -10,8 +10,7 @@ export type Todo = {
 
 function App() {
   const [todos, setTodos] = useState<Todo[]>([]);
-  const [todoList, setTodoList] = useState(
-  {
+  const [todoList, setTodoList] = useState({
     id: todos.length,
     title: "",
     content: "",
@@ -20,10 +19,11 @@ function App() {
   })
 
   //フィルター
-  const [filter, setFilter] = useState<Filter>('all');
+  // const [filter, setFilter] = useState<Filter>('all');
 
-  const onChangeTodo = (e:any) => setTodoList(e.target.value);
-  // const onChangeContent = (e: React.ChangeEvent<HTMLInputElement>) => setTodoList(e.target.value);
+  //インプットフォームの状態を管理
+  const onChangeTitle = (e:any) => setTodoList(e.target.value);
+  const onChangeContent = (e:any) => setTodoList(e.target.value);
 
   //追加機能
   const onClickAdd = (e: { preventDefault: () => void; }) => {
@@ -40,56 +40,58 @@ function App() {
     // }
 
     setTodos([todoList, ...todos]);
-    // setTodoList();
+    //入力後空にする
+    // setTodoList({});
   };
 
   //削除機能
-  const onClickDelete = (id: number) => {
-    setTodos(todos.filter((todo) => todo.id !== id));
-  }
+  // const onClickDelete = (id: number) => {
+  //   setTodos(todos.filter((todo) => todo.id !== id));
+  // }
 
   //編集機能
-  const onClickEdit = (id: number, title: string) => {
-    const newTodo = todos.map((todo) => {
-      if (todo.id === id) {
-        todo.title = title;
-      }
-      return todo;
-    })
-    setTodos(newTodo);
-  };
+  // const onClickEdit = (id: number, title: string, content: string) => {
+  //   const newTodo = todos.map((todo) => {
+  //     if (todo.id === id) {
+  //       todo.title = title;
+  //       todo.content = content;
+  //     }
+  //     return todo;
+  //   })
+  //   setTodos(newTodo);
+  // };
 
   //チェックボックスがチェックされた時(ステータス機能)
-  const onClickCheck = (id: number, checked: boolean) => {
-    const newTodo = todos.map((todo) => {
-      if (todo.id === id) {
-        todo.checked = !checked;
-      }
-      return todo;
-    })
-    setTodos(newTodo);
-  }
+  // const onClickCheck = (id: number, checked: boolean) => {
+  //   const newTodo = todos.map((todo) => {
+  //     if (todo.id === id) {
+  //       todo.checked = !checked;
+  //     }
+  //     return todo;
+  //   })
+  //   setTodos(newTodo);
+  // }
 
   //絞り込み機能
   //　全てのtodo、完了したtodo、未完了のtodo
-  type Filter = "all" | "checked" | "unchecked";
+  // type Filter = "all" | "checked" | "unchecked";
 
-  const filterTodos = todos.filter((todo) => {
-    //filterステートの値に応じて異なる配列を返す
-    switch (filter) {
-      case "all":
-        // 全てのtodo
-        return todo;
-      case "checked":
-        //完了したtodo
-        return todo.checked;
-      case "unchecked":
-        //未完了のtodo
-        return !todo.checked;
-      default:
-        return todo;
-    }
-  })
+  // const filterTodos = todos.filter((todo) => {
+  //   //filterステートの値に応じて異なる配列を返す
+  //   switch (filter) {
+  //     case "all":
+  //       // 全てのtodo
+  //       return todo;
+  //     case "checked":
+  //       //完了したtodo
+  //       return todo.checked;
+  //     case "unchecked":
+  //       //未完了のtodo
+  //       return !todo.checked;
+  //     default:
+  //       return todo;
+  //   }
+  // })
 
   return (
     <div className="App">
@@ -99,27 +101,27 @@ function App() {
           <input
             type="text"
             placeholder="タイトル"
-            onChange={onChangeTodo}
+            onChange={onChangeTitle}
           />
         <p>内容</p>
           <input
             type="text"
             placeholder="内容"
-            onChange={onChangeTodo}
+            onChange={onChangeContent}
           />
         <p><button onClick={onClickAdd}>追加</button></p>
       </form>
     <div>
       <h2>Todo一覧</h2>
         <div>
-          <select
+          {/* <select
             defaultValue="all"
             onChange={(e) => setFilter(e.target.value as Filter)}
           >
             <option value="all">全てのtodo</option>
             <option value="checked">完了したtodo</option>
             <option value="unchecked">未完了のtodo</option>
-          </select>
+          </select> */}
         </div>
         <table>
           <thead>
@@ -130,7 +132,8 @@ function App() {
             </tr>
           </thead>
           <tbody>
-            {filterTodos.map((todo, index) =>(
+            {/* {filterTodos.map((todo, index) =>( */}
+            {todos.map((todo, index) =>(
               <tr key={index}>
                 <td>{index + 1}</td>
                 <td>
@@ -138,7 +141,7 @@ function App() {
                     type="text"
                     disabled={todo.checked}
                     value={todo.title}
-                    onChange={(e) => onClickEdit(todo.id, e.target.value)}
+                    // onChange={(e) => onClickEdit(todo.id, todo.title, e.target.value)}
                   />
                 </td>
                 <td>
@@ -146,17 +149,17 @@ function App() {
                     type="text"
                     disabled={todo.checked}
                     value={todo.content}
-                    onChange={(e) => onClickEdit(todo.id, e.target.value)}
+                    // onChange={(e) => onClickEdit(todo.id, todo.content ,e.target.value)}
                   />
                 </td>
                 <td>
                   <input
                     type="checkbox"
-                    onChange={() => onClickCheck(todo.id, todo.checked)}
+                    // onChange={() => onClickCheck(todo.id, todo.checked)}
                   />
                 </td>
                 <td>
-                  <button onClick={() => onClickDelete(todo.id)}>削除</button>
+                  {/* <button onClick={() => onClickDelete(todo.id)}>削除</button> */}
                 </td>
               </tr>
             ))}
